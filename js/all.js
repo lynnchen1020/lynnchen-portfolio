da172e8904d95180574b23adad641c5080d8c217
LynnChen <lynnchen1020@gmail.com>
2016-08-25 16:38:37 +0800

    update sec version

--- js/all.js
+++ js/all.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-    //手機menu下拉
+    // 關於我淡入效果
+    $('.about').delay(100).animate({
+        opacity: 1
+    });
+    //手機 menu 下拉
     $(function() {
         $('.menu-btn').on('click', fadeOpen);
-
         function fadeOpen(e) {
             e.preventDefault();
             $('.menu-list').stop().fadeToggle().toggleClass('active');
@@ -15,7 +18,7 @@ $(document).ready(function() {
             };
         };
     });
-    //點擊menu滾動至各區域
+    //點擊 menu 滾動至各區域
     $('.scroll-btn').on('click', function(e) {
         e.preventDefault();
         var hookStr = $(this).attr('href');
@@ -24,20 +27,29 @@ $(document).ready(function() {
             scrollTop: top
         }, 500);
     });
-    //回到頂端按鈕
+    // 淡入gotop按鈕 & 回到頂端事件
     $(window).scroll(function() {
-        //console.log($('body').scrollTop());
         if ($('body').scrollTop() > 750) {
             $('.gotop-btn').fadeIn(100);
         } else {
             $('.gotop-btn').fadeOut(100);
         }
     });
-    $('.gotop-btn').on('click', function(e) {
+     $('.gotop-btn').on('click', function(e) {
         e.preventDefault();
         $('body').animate({
             scrollTop: 0
         });
+    });
+    //視窗滾動各區內容淡入 
+    $(window).scroll(function(){
+        var arr = ['.experience', '.skill', '.portfolio', '.contact'];
+        for(i=0; i<arr.length; i++){
+            var offsetTop = $(arr[i]).offset().top;
+            if($('body').scrollTop() > offsetTop-250){
+            $(arr[i]).css('opacity', '1');
+            }
+        }
     });
     // portfolio點擊顯示說明框
     $('figure').click(function(){
