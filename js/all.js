e4813fb87b8c83c0706356804e6ab2dcf4f39028
LynnChen <lynnchen1020@gmail.com>
2016-08-25 16:34:44 +0800

    Revert "sec version"
    This reverts commit b9e4b2adf29ed3b29ef1470203a6404f117eb307.

--- js/all.js
+++ js/all.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
-    // 關於我淡入效果
-    $('.about').delay(100).animate({
-        opacity: 1
-    });
-    //手機 menu 下拉
+    //手機menu下拉
     $(function() {
         $('.menu-btn').on('click', fadeOpen);
+
         function fadeOpen(e) {
             e.preventDefault();
             $('.menu-list').stop().fadeToggle().toggleClass('active');
@@ -18,7 +15,7 @@ $(document).ready(function() {
             };
         };
     });
-    //點擊 menu 滾動至各區域
+    //點擊menu滾動至各區域
     $('.scroll-btn').on('click', function(e) {
         e.preventDefault();
         var hookStr = $(this).attr('href');
@@ -27,29 +24,20 @@ $(document).ready(function() {
             scrollTop: top
         }, 500);
     });
-    // 淡入gotop按鈕 & 回到頂端事件
+    //回到頂端按鈕
     $(window).scroll(function() {
+        //console.log($('body').scrollTop());
         if ($('body').scrollTop() > 750) {
             $('.gotop-btn').fadeIn(100);
         } else {
             $('.gotop-btn').fadeOut(100);
         }
     });
-     $('.gotop-btn').on('click', function(e) {
+    $('.gotop-btn').on('click', function(e) {
         e.preventDefault();
         $('body').animate({
             scrollTop: 0
         });
-    });
-    //視窗滾動各區內容淡入 
-    $(window).scroll(function(){
-        var arr = ['.experience', '.skill', '.portfolio', '.contact'];
-        for(i=0; i<arr.length; i++){
-            var offsetTop = $(arr[i]).offset().top;
-            if($('body').scrollTop() > offsetTop-250){
-            $(arr[i]).css('opacity', '1');
-            }
-        }
     });
     // portfolio點擊顯示說明框
     $('figure').click(function(){
